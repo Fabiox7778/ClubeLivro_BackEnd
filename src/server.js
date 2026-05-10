7717ce41d30a5ff30dab6432684ccce70987b003
Murilow <milanmurilo7@gmail.com>
2026-05-10 00:22:09 -0300

    feat: adiciona a rota de usuarios ao projeto

--- src/server.js
+++ src/server.js
@@ -3,6 +3,7 @@ import 'dotenv/config';
 import personagemRoutes from './routes/personagemRoute.js';
 import sobreRoutes from './routes/sobreRoute.js';
 import equipeRoutes from './routes/equipeRoute.js';
+import usuarioRoutes from './routes/usuarioRoute.js'
 import { apiKey } from './lib/middleware/apiKey.js';
 
 
@@ -19,6 +20,7 @@ app.get('/', (req, res) => {
 app.use('/api/personagem',apiKey, personagemRoutes);
 app.use('/api/sobre', apiKey, sobreRoutes);
 app.use('/api/equipe', apiKey, equipeRoutes);
+app.use('/api/usuario',apiKey, usuarioRoutes);
 
 app.use((req, res) => {
     res.status(404).json({ error: 'Rota não encontrada' });
