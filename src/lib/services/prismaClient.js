b6d24ad07e9744e39bf9ec39ba89ed9e8fa91fc6
Murilow <milanmurilo7@gmail.com>
2026-06-06 00:03:51 -0300

    feat: adiciona novas informações no seed

--- src/lib/services/prismaClient.js
+++ src/lib/services/prismaClient.js
@@ -1,11 +1,13 @@
 import pg from 'pg';
 import 'dotenv/config';
-import pkg from '@prisma/client';
+import { PrismaClient } from '@prisma/client';
 import { PrismaPg } from '@prisma/adapter-pg';
 
-const { PrismaClient } = pkg;
+if (!process.env.DATABASE_URL) {
+    throw new Error('DATABASE_URL não está definida');
+}
 
-const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL, });
+const pool = new pg.Pool({ connectionString: process.env.DATABASE_URL });
 const adapter = new PrismaPg(pool);
 const prisma = new PrismaClient({ adapter });
 
